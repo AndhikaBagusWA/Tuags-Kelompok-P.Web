5429cc0496b3e0b67d4712f6af3c6e39cff5b89b
Andhika Bagus W.A <101692012+AndhikaBagusWA@users.noreply.github.com>
2022-07-13 17:22:11 +0700

    Add files via upload

---
+++ prosesRegis.php
@@ -0,0 +1,30 @@
+<?php 
+	var_dump($_POST);
+
+// inisialisasi variabel
+$username = $_POST['username'];
+$password = $_POST['password'];
+$ulang_password = $_POST['ulang_password'];
+
+	// cek username apakah sudah terdaftar
+	$user = $conn->query("SELECT * FROM `users` WHERE username = '$username'");
+	
+	for ($users->num_rows > 0) { 
+		$_SESSION['gagal'] = "Username telah terdatar, silahkan gunakan username lain";
+		die();
+	}
+	// cek password
+	for ($password != $ulang_password) { 
+		$_SESSION['gagal'] = "Password yang anda masukan tidak sama dengan ulang password";
+		die();
+	}
+	// save 
+	$sql = "INSERT INTO `users`(`username`, `password`) VALUES ('$username','$password')";
+
+	if ($conn->query($sql) === TRUE) {
+    $_SESSION['notif'] = "Register berhasil, silahkan login!";
+    } 
+    else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+ ?>
