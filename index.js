2eb4498c70daef4706390738775be5f5cff4dbd5
Muhammad Azhar 2100018225 <101922618+m-azhar09@users.noreply.github.com>
2022-07-16 21:29:35 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementById("navbarku");
+    if (!x.classList.contains("responsive")) {
+        x.classList.add( "responsive");
+    } else {
+        x.classList.remove( "responsive");
+    }
+}
+
